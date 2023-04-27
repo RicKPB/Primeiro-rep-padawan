36c36928a3ed53f4ec73d056274e43b095a6077f
RicKPB <papeschihenrique0@gmail.com>
2023-04-27 02:28:38 -0300

    Criação dos comandos no index.html e no js.js

---
+++ missao-estrelar/js/js.js
@@ -0,0 +1,15 @@
+//Declaracao de variaveis
+var missao = "Missão Estelar JS em execução!!!"
+
+//Evento OnLoad
+function eventoOnLoad(){
+    console.log("Evento OnLoad disparado");
+}
+
+//Evento OnClick
+function eventoOnClick(){
+
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+    alert("Missão Estelar JS realizada com sucesso!!!");
+}
+
